d6e7b3a1dfaa9597dc7b6c2d1e94315f308f0b81
cwerts <cwerts01@gmail.com>
2020-12-05 15:47:35 -0500

    Beginning Implementation for Hash Map

--- main.c
+++ main.c
@@ -3,19 +3,48 @@
 #include <pthread.h>
 
 #define BUFFER_SIZE 32
+#define MAP_SIZE 8
 
-int main(int argc, char * argv[]) {
-    char input[BUFFER_SIZE];
+/* Node Structure that the HashMap will contain */
+typedef struct {
+    int key;
+    char fileName[];
+    char contents[];
+    node * next;
+} node;
 
+/*Structure for the actual Map that will hold the nodes, max of 8 */
+typedef struct {
+    int size = MAP_SIZE;
+    node * headNode;
+} map;
 
-
+int getHashKey(char fileName[]) {
+    int hashCode = 0;
+    int length = 0;
+    for(int i = 0; i < BUFFER_SIZE; i++) {
+        if(fileName[i] == "/n" || fileName[i] == "/0") {
+            break;
+        }
+        hashCode += fileName[i] * 19^(2 * i - i);
+        length = i;
+    }
+    return hashCode % length;
 }
-
 
 void * loadFile(void * fileName) {
 
 }
 
 void * deleteCache(void * fileName) {
+
+}
+
+
+
+int main(int argc, char * argv[]) {
+    char input[BUFFER_SIZE];
+
+
 
 }
