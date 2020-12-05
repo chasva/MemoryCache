8519a3ae01ae4500f2f24e148cb54be3a7d23e50
cwerts <cwerts01@gmail.com>
2020-12-05 14:16:20 -0500

    Basic Stuff

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 32
 
@@ -6,5 +8,14 @@ int main(int argc, char * argv[]) {
     char input[BUFFER_SIZE];
 
 
+
+}
+
+
+void * loadFile(void * fileName) {
+
+}
+
+void * deleteCache(void * fileName) {
 
 }
