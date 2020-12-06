eabb3b729605305ece8932a6f6c73ed170776f50
cwerts <cwerts01@gmail.com>
2020-12-06 18:22:22 -0500

    Added Node Storage, Finishing add to table method.
    General Bug Fixes too

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 32
 #define MAP_SIZE 8
@@ -20,30 +21,30 @@ typedef struct {
     char * fileName;
     char * size;
     char * contents;
-    node * next;
+    void * next;
 } node;
 
 /*Structure for the actual Map that will hold the nodes, max of 8 */
 typedef struct {
-    int size = MAP_SIZE;
+    int size;
     node * headNode;
 } table;
 
 /*Method to Create the Table that will be Global Variable */
 table createTable() {
     table t;
-    node tempHead;
-    tempHead.key = -1;
-    t.headNode = tempHead;
+    t.size = MAP_SIZE;
+    node tempHead = {-1, NULL, NULL, NULL, NULL, NULL};
+    t.headNode = &tempHead;
     return t;
 }
 /*Global Variable so all threads can access it */
-table hashTable = createTable();
+table hashTable;
 
 /*Returns a unique HashKey based on Filename */
 int getHashKey(char * fileName) {
     int hashCode = 0;
-    length = strlen(fileName);
+    int length = strlen(fileName);
     char name[length];
     strcpy(name, fileName);
 
@@ -56,8 +57,6 @@ int getHashKey(char * fileName) {
     }
     return hashCode % length;
 }
-
-void addToTable(char * fileName, int size, char * content)
 
 /*Returns the size and contents of a file in format n:[contents] n being size, if no contents then it returns 0: */
 char * loadContents(int key) {
@@ -78,14 +77,14 @@ char * loadContents(int key) {
 
             return loadedContent;
         }
-        if(!(currentNode.next != NULL)) {
+        if(currentNode.next == NULL) {
             break;
         }
-        currentNode = *currentNode.next;
+        currentNode = *(node *)currentNode.next;
     }
 
-    char * empty = "0:"
-    return char empty;
+    char * empty = "0:";
+    return empty;
 }
 
 //Used to remove the file node and link one ahead to the rest
@@ -94,10 +93,23 @@ void removeNode(int key) {
     node currentNode = *hashTable.headNode;
     if(currentNode.key = key) {
         if(currentNode.next != NULL) {
-            hashTable.headNode = currentNode.next;
+            //Assign the new head node
+            hashTable.headNode = (node *)currentNode.next;
+
+            //Free the memory of the Old Head
+            free(currentNode.contents);
+            free(currentNode.fileName);
+            free(currentNode.size);
+            return;
         }
+        //If there are no elements behind the head
         else {
+            //Remembering to free the memory
+            free(currentNode.contents);
+            free(currentNode.fileName);
+            free(currentNode.size);
             hashTable = createTable();
+            return;
         }
     }
 
@@ -108,17 +120,17 @@ void removeNode(int key) {
     for(int i = 0; i < MAP_SIZE; i++) {
         //Continue only if there is a next node
         if(currentNode.next != NULL)
-            nextNode = *currentNode.next;
+            nextNode = *(node *)currentNode.next;
         else
             return;
 
 
         //Enter if it is the node to remove
-        if(key == nextNodeNode.key) {
+        if(key == nextNode.key) {
             //Free the memory originally allocated for the pointers
             free(currentNode.contents);
             free(currentNode.fileName);
-            free(currentNOde.size);
+            free(currentNode.size);
 
             //Link the previous node to next one if there is to not lose the chain
             if(nextNode.next != NULL)
@@ -128,9 +140,49 @@ void removeNode(int key) {
 
         }
         //Move the node up
-        currentNode = *currentNode.next;
+        currentNode = *(node *)currentNode.next;
 
     }
+}
+
+//MEthod to add nodes to the hash table
+void addToTable(node newNode) {
+    node currentNode = *hashTable.headNode;
+    for(int i = 0; i < MAP_SIZE; i++) {
+        //Check for Collision and Remove it
+        if(currentNode.key == newNode.key) {
+            //Replace the Node, delete it, and return
+            return;
+        }
+        //Check if the empty node
+        if(currentNode.next == NULL) {
+            //Link to next node
+
+            //Check how long the list is then delete head if necessary
+            if(i >= MAP_SIZE - 1) {
+                //Will delete the head and assign the node as necessary
+                removeNode(getHashKey(hashTable.headNode->fileName));
+            }
+        }
+        //Break at this point
+        if(currentNode.next == NULL) {
+            break;
+        }
+        //Continue through loop
+        currentNode = *(node *)currentNode.next;
+    }
+
+}
+//Create the new Node
+void createNode(char * input[]) {
+    node newNode;
+    newNode.fileName = input[0];
+    newNode.key = getHashKey(newNode.fileName);
+    newNode.size = input[1];
+    newNode.contents = input[2];
+
+    addToTable(newNode);
+
 }
 
 //make sure that the lock should be applied to all of these methods and not just delete and load
@@ -139,24 +191,34 @@ void * loadFile(void * fileName) {
     pthread_mutex_lock(&lock);
     //use the hash key to return the contents of the file
     char * file = loadContents(key);
+    //Release lock
     pthread_mutex_unlock(&lock);
     return (void *)file;
+
+    /*TODO Remember to free the memory that was allocated for file
+     * This should be done after it is sent back to dispatcher or printed or whatever
+     * needs to be done*/
 }
 
 void * deleteCache(void * fileName) {
-    pthread_mutex_lock(&lock);
-    //take in the file name, get the key, and then remove the file from the cache
     int hashKey = getHashKey(fileName);
 
+    pthread_mutex_lock(&lock);
     //remove the file from the cache using the key
-
+    removeNode(hashKey);
+    //release lock
     pthread_mutex_unlock(&lock);
 }
 
-void * storeToCache(){
+void * storeToCache(void * input){
+    //Get the Filename, Size, and Contents store it in char * arr called data.
+    char * data[3];
+    for(int i = 0; i < 3; i++) {
+        data[i] = ((char *)input)[i];
+    }
     pthread_mutex_lock(&lock);
-    //store the contents to the cache - create a key
 
+    createNode(data);
 
     pthread_mutex_unlock(&lock);
 }
@@ -214,6 +276,8 @@ void messageReceived(char * receiveLine){
 int main(int argc, char * argv[]) {
     //initialize the mutex that will be used for the methods (add error handling?)
     pthread_mutex_init(&lock, NULL);
+    //initialize the hash table
+    hashTable = createTable();
 
     //start tcp connection and have it always listening for dispatcher *********
     int serverSocket, bytesRead;
